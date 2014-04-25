947bf1a9e8548fd9865f00c6e4fa16bf867b3124
unbelt <unbelt@outlook.com>
2014-04-25 16:11:50 +0300

    C# Basics - Exam
    C# Basics - Exam (11 April 2014 - Evening)

---
+++ Exams/CSharp-Basics-Exam-(11-April-2014-Evening)/02.Task/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+class Illuminati
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        BigInteger sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            switch (input[i].ToString().ToLowerInvariant())
+            {
+                case "a": sum += 65;
+                    count++;
+                    break;
+                case "e": sum += 69;
+                    count++;
+                    break;
+                case "i": sum += 73;
+                    count++;
+                    break;
+                case "o": sum += 79;
+                    count++;
+                    break;
+                case "u": sum += 85;
+                    count++;
+                    break;
+            }
+        }
+
+        Console.WriteLine(count);
+        Console.WriteLine(sum);
+    }
+}
